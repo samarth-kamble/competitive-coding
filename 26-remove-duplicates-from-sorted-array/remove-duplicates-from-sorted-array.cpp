f829e28dff5e67c8ec841926d303ebb418193f2e
Samarth Kamble <145892043+samarth-kamble@users.noreply.github.com>
2026-07-23 20:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (98.28%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+
+        int i = 1 ;
+
+        for (int j = 1 ; j < nums.size() ; j++) {
+            if (nums[j] != nums[i-1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
